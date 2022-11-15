6b21dd6e4ca35d6de33b80733a9d481b39d90561
avsaresra <117168517+avsaresra@users.noreply.github.com>
2022-11-15 14:37:21 +0300

    Create ocr.py

---
+++ ocr.py
@@ -0,0 +1,15 @@
+import streamlit as st  #Web Uygulaması
+from PIL import Image #Resim dosyalarını işlemek için kullanıyoruz
+import pytesseract #package.txt dosyasındaki tesseract modüllerinin pythonda çalışabilmesi için gerekiyor
+import base64 #ocr ile oluşturulmuş metinleri dosya haline getirmek için kullandığımız kütüphane
+import os
+import fitz #pymupdf modülünü çağırmak için kullanıyoruz
+
+
+#title
+st.title("Optik Karakter Okuyucu - Resim ve PDF dosyalarından metin çıkarımı")
+
+#subtitle
+st.markdown("## `streamlit` ve `tesseract` kullanarak geliştirilmiş bir OCR Web uygulaması")
+
+st.markdown("")
